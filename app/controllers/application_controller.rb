d68a95793be3e871e6c28f6ca9052cc0a7794823
Mariya <meari.kachusova@mail.ru>
2019-04-24 11:27:47 +0300

    Rubocop. Gitignore was modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-	
-	def authenticate_admin_user
+  def authenticate_admin_user
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV.fetch('ADMIN_NAME') && password == ENV.fetch('ADMIN_PASSWORD')
     end
   end
-
 end
